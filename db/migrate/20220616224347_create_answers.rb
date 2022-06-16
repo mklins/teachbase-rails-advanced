59a9efa22f6f65133c86b3e4d696e03f89d13a53
Mykola <mklins@tuta.io>
2022-06-17 01:46:13 +0300

    Generated Question and Answer models

---
+++ db/migrate/20220616224347_create_answers.rb
@@ -0,0 +1,10 @@
+class CreateAnswers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :answers do |t|
+      t.belongs_to :question
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
