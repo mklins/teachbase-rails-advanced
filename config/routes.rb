db0b398d1bfd8e9d8160c5d9225ee617f47c44a5
Mykola <mklins@tuta.io>
2022-06-20 18:28:52 +0300

    Controller for questions and tests for it

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "questions#index"
+  resources :questions
+    resources :answers
 end
